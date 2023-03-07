461efb032998bdc7a0c8f158413a8aba3eb0871e
luuduocloan <duocloan2004qb@gmail.com>
2023-03-07 12:42:38 +0700

    first commit

---
+++ Bai1.3.2.py
@@ -0,0 +1,7 @@
+print('You will be'+str(int(my age)+1)+'in a year')
+print('You will be'+str(int('4')+1)+'in a year')
+print('You will be'+str(4+1)+'in a year')
+print('You will be'+str(5)+'in a year')
+print('You will be'+'5'+'in a year')
+print('You will be 5'+'in a year')
+print('You will be 5 in a year')
